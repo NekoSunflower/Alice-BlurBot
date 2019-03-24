16dc021529d76783971108e165922a6dd3c33931
yiweigang <522240446@qq.com>
2019-03-25 02:08:36 +0800

    fix demo

--- first-init.sh
+++ first-init.sh
@@ -1,4 +1,11 @@
-yum -y install python-pip
+cd ~/
+mkdir alice-blurbot
+cd alice-blurbot
+wget https://github.com/NekoSunflower/Alice-BlurBot/archive/master.zip -O alice-blurbot.zip
+unzip alice-blurbot.zip
+mv Alice-BlurBot-master/* ./
+rm -rf Alice-BlurBot-master
+yum -y install python-pip python-devel
 python -m pip install --upgrade pip
 pip install -U opencv-python
 pip install -U numpy
@@ -7,3 +14,6 @@ pip install -U cython
 cd utils/bbox
 chmod +x make.sh
 ./make.sh
+cd ~/alice-blurbot
+wget https://image.moeclub.org/GoogleDrive/1HcZuB_MHqsKhKEKpfF1pEU85CYy4OlWO -O checkpoints_mlt.zip
+unzip checkpoints_mlt.zip
